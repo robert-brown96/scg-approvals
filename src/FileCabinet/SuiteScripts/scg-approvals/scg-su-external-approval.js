db270f80f00d077d9b9f536f73d523f1854c6fb9
Robert Brown <brownrdatx@gmail.com>
2022-12-21 19:59:31 -0600

    alerian objects

--- src/FileCabinet/SuiteScripts/scg-approvals/scg-su-external-approval.js
+++ src/FileCabinet/SuiteScripts/scg-approvals/scg-su-external-approval.js
@@ -15,10 +15,10 @@ define(["N/record", "N/ui/serverWidget", "N/error", "N/workflow"], (
       * @param {ServerResponse} context.response - Suitelet response
       * @since 2015.2
       */
-     const onRequest = async context => {
+     const onRequest = context => {
           try {
-               if (context.request.method === "GET") await _get(context);
-               else await _post(context);
+               if (context.request.method === "GET") _get(context);
+               else _post(context);
           } catch (e) {
                log.error({
                     title: "onRequest: ERROR",
@@ -34,19 +34,21 @@ define(["N/record", "N/ui/serverWidget", "N/error", "N/workflow"], (
       * @param {ServerResponse} context.response - Suitelet response
       * @since 2015.2
       */
-     const _get = async context => {
-          let form;
-          await Promise.all(
-               params.map(p => {
-                    if (!Object.keys(context.request.parameters).includes(p))
-                         throw error.create({
-                              name: "MISSING_REQUIRED_ARG",
-                              message: `MISSING REQUIRED ARG ${p}`,
-                              notifyOff: false
-                         });
-               })
-          );
-          const { action, tran, approver, next, recType } =
+     const _get = context => {
+          let form = serverWidget.createForm({
+               title: "Purchase Approval"
+          });
+
+          params.forEach(p => {
+               if (!Object.keys(context.request.parameters).includes(p))
+                    throw error.create({
+                         name: "MISSING_REQUIRED_ARG",
+                         message: `MISSING REQUIRED ARG ${p}`,
+                         notifyOff: false
+                    });
+          });
+
+          const { action, tran, approver, next, recType, state } =
                context.request.parameters;
 
           if (action === "APPROVE") {
@@ -57,17 +59,13 @@ define(["N/record", "N/ui/serverWidget", "N/error", "N/workflow"], (
 
                // TODO: remove hard coded record type
                workflow.trigger({
-                    actionId: action,
+                    actionId: next,
                     recordId: tran,
-                    recordType: record.Type.VENDOR_BILL,
-                    workflowId: 28
+                    recordType: recType,
+                    workflowId: "customworkflow_scg_vb_wf",
+                    stateId: state
                });
 
-               record.submitFields({
-                    type: recType,
-                    id: parseInt(tran),
-                    values: { custbody_scg_approval_s_hide: parseInt(next) }
-               });
                form = serverWidget.createForm({
                     title: "Purchase Approved",
                     hideNavBar: true
@@ -75,15 +73,52 @@ define(["N/record", "N/ui/serverWidget", "N/error", "N/workflow"], (
                const approvedFld = form.addField({
                     id: "custpage_message",
                     type: serverWidget.FieldType.INLINEHTML,
-                    label: "Approved Successfully"
+                    label: "Approved"
                });
-               approvedFld.setDefaultValue(
-                    '<font size="8"><b>Purchase Order Approved Successfully</b></font>'
-               );
+               approvedFld.defaultValue =
+                    '<font size="8"><b>Purchase Approved Successfully</b></font>';
           } else if (action === "REJECT") {
                log.debug({
                     title: `REJECT TRANSACTION ${tran}`,
                     details: `next stage is ${next} by ${approver}`
+               });
+
+               // TODO: remove hard coded record type
+               workflow.trigger({
+                    actionId: next,
+                    recordId: tran,
+                    recordType: recType,
+                    workflowId: "customworkflow_scg_vb_wf",
+                    stateId: state
+               });
+
+               form.addSubmitButton({
+                    label: "Submit"
+               });
+               const rejectMessageFld = form.addField({
+                    id: "custpage_reject_message",
+                    type: serverWidget.FieldType.LONGTEXT,
+                    label: "Rejection Message"
+               });
+               rejectMessageFld.isMandatory = true;
+               const tranFld = form.addField({
+                    id: "custpage_reject_tran",
+                    type: serverWidget.FieldType.TEXT,
+                    label: "Rejection Transaction"
+               });
+               tranFld.defaultValue = tran;
+               tranFld.updateDisplayType({
+                    displayType: serverWidget.FieldDisplayType.HIDDEN
+               });
+
+               const tranTypeFld = form.addField({
+                    id: "custpage_reject_tran_type",
+                    type: serverWidget.FieldType.TEXT,
+                    label: "Rejection Transaction Type"
+               });
+               tranTypeFld.defaultValue = recType;
+               tranTypeFld.updateDisplayType({
+                    displayType: serverWidget.FieldDisplayType.HIDDEN
                });
           } else
                throw error.create({
@@ -102,11 +137,36 @@ define(["N/record", "N/ui/serverWidget", "N/error", "N/workflow"], (
       * @param {ServerResponse} context.response - Suitelet response
       * @since 2015.2
       */
-     const _post = async context => {
+     const _post = context => {
           log.debug("post", context);
+          const form = serverWidget.createForm({
+               title: "Purchase Rejected"
+          });
+          const {
+               custpage_reject_message,
+               custpage_reject_tran,
+               custpage_reject_tran_type
+          } = context.request.parameters;
+
+          record.submitFields({
+               type: custpage_reject_tran_type,
+               id: custpage_reject_tran,
+               values: {
+                    custbody_scg_approval_rej_message: custpage_reject_message
+               }
+          });
+
+          const approvedFld = form.addField({
+               id: "custpage_message",
+               type: serverWidget.FieldType.INLINEHTML,
+               label: "Approved"
+          });
+          approvedFld.defaultValue =
+               '<font size="8"><b>Purchase Rejected Successfully</b></font>';
+          context.response.writePage(form);
      };
 
-     const params = ["action", "tran", "approver", "next", "recType"];
+     const params = ["action", "tran", "next", "recType", "state"];
 
      return { onRequest };
 });
