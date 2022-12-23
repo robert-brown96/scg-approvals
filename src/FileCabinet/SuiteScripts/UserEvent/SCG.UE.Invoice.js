de80919f15db060ce2f5955b8dc2e5ba8d3f1d7a
Robert Brown <brownrdatx@gmail.com>
2022-12-22 19:48:18 -0600

    fix invoice ue

--- src/FileCabinet/SuiteScripts/UserEvent/SCG.UE.Invoice.js
+++ src/FileCabinet/SuiteScripts/UserEvent/SCG.UE.Invoice.js
@@ -1,23 +1,32 @@
 /**
  * User Events for Invoice
  *
- * @NApiVersion 2.0
+ * @NApiVersion 2.1
  * @NScriptType UserEventScript
  * @author Nick Weeks
  */
 define(["N/record", "N/search"], function (record, search) {
      function beforeSubmit(context) {
-          var curRec = context.newRecord;
-          var billToAddress = curRec.getValue({
+          if (
+               context.type !== context.UserEventType.EDIT &&
+               context.type !== context.UserEventType.XEDIT
+          )
+               return;
+          const curRec = context.newRecord;
+          const billToAddress = curRec.getValue({
                fieldId: "billingaddress_key"
           });
+          log.debug({
+               title: "Bill To",
+               details: billToAddress
+          });
           if (!billToAddress) return;
-          var fieldLookUp = search.lookupFields({
+          const fieldLookUp = search.lookupFields({
                type: "address",
                id: billToAddress,
                columns: ["custrecord_scg_email"]
           });
-          var emailStr = fieldLookUp.custrecord_scg_email;
+          const emailStr = fieldLookUp.custrecord_scg_email;
           log.debug("Emails to send to", emailStr);
           curRec.setValue({
                fieldId: "custbody_invoice_email_address_list",
@@ -30,7 +39,58 @@ define(["N/record", "N/search"], function (record, search) {
                });
           }
      }
+
+     const afterSubmit = context => {
+          if (context.type !== context.UserEventType.CREATE) return;
+
+          const { email, emailSetting } = getAddressEmails(context);
+
+          const valObj = {};
+
+          if (emailSetting) valObj.custbody_invoice_delivery_type = 1;
+          if (email) valObj.custbody_invoice_email_address_list = email;
+
+          record.submitFields({
+               type: record.Type.INVOICE,
+               id: context.newRecord.id,
+               values: valObj
+          });
+     };
+
+     /**
+      *
+      * @param context
+      * @returns {{emailSetting: (*|number|Date|string|Array|boolean), email: (*|number|Date|string|Array|boolean)}}
+      */
+     const getAddressEmails = context => {
+          try {
+               const billAddRec = context.newRecord.getSubrecord({
+                    fieldId: "billingaddress"
+               });
+               if (!billAddRec) return;
+
+               const email = billAddRec.getValue({
+                    fieldId: "custrecord_scg_email"
+               });
+               const emailSetting = billAddRec.getValue({
+                    fieldId: "custrecord_email_invoice"
+               });
+
+               log.debug({
+                    title: `email: ${emailSetting}`,
+                    details: email
+               });
+
+               return { email, emailSetting };
+          } catch (e) {
+               log.error({
+                    title: "Error setting emails",
+                    details: e
+               });
+          }
+     };
      return {
-          beforeSubmit: beforeSubmit
+          beforeSubmit,
+          afterSubmit
      };
 });
